c0245eeb749912d4fd7c827b1af6fc965a03c54e
Lucas Alves Pinheiro <l.pinheiro.w@gmail.com>
2025-05-19 15:05:24 -0300

    Adição do Figma e fonte

--- src/componentes/mainComponents/MyGallery.jsx
+++ src/componentes/mainComponents/MyGallery.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFigma } from '@fortawesome/free-brands-svg-icons'
 
 
 const MyGallery = () => {
   return (
-    <div className='gallery'>Work In Progress</div>
+    <>
+      <div className='gallery'>Work In Progress</div>
+
+      <br />
+
+      <a className="footer__links--a" href='https://www.figma.com/proto/0MWjKzgncI7kq4UZDTAHPR/Portf%C3%B3lio---Lucas-Alves-Pinheiro?page-id=3009%3A714&node-id=3012-828&viewport=1748%2C-1064%2C0.76&t=nznWeCIX4YsBytc3-1&scaling=scale-down-width&content-scaling=fixed&starting-point-node-id=3012%3A828'>
+        <div className='figma'>
+          <FontAwesomeIcon className="icons" icon={faFigma} />
+          <p >Figma do projeto</p> 
+        </div>
+      </a>
+    </>
   )
 }
 
